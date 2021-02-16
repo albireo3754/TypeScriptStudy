ad09b20c2dc524c53953755171658ff7b8ffa9b8
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-02-16 22:32:42 +0900

    type inference test

--- study/variable.ts
+++ study/variable.ts
@@ -28,3 +28,31 @@ const logNumber: (i: number) => string = (i: number) => {
   return '3'
 }
 logNumber(3)
+
+//Whento use annotations
+const json = '{"x": 10}'
+const coordinates = JSON.parse(json);
+console.log(coordinates)
+
+// 2) When we declarea variable on one line
+
+let words = ['red', 'green'];
+let foundWord: boolean;
+for (let i = 0; i < words.length; i++) {
+  if (words[i] == 'green') {
+    foundWord = true;
+  }
+}
+class Do {
+
+}
+//3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, new Do()];
+// let numberAboveZero = false; 이렇게하면 type inference에 의해 알아서 false가됨
+// let numberAboveZero: boolean | number = false;
+// let numberAboveZero: typeof point = new Car();
+// for (let i = 0; i< numbers.length; i++) {
+//   numberAboveZero = numbers[i];
+//   console.log(numbers[i])
+// }
+console.log(numberAboveZero)
