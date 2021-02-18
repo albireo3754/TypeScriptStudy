a01d61c8f50f920231d1d84ae637dd6353489714
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-02-19 00:40:29 +0900

    array.ts

---
+++ study/array.ts
@@ -0,0 +1,11 @@
+const carMakers: string[] = ['ford', 'toyota']
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+  ['f150'],['camaro']
+]
+
+// Help with inference when extracting values
+
+const car = carMakers[0];
+console.log(car)
