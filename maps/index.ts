546dd222e4896794cd9a5e01d04140006a94c5e8
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-02-24 00:13:04 +0900

    not good method of making marker, because of duplication

--- maps/index.ts
+++ maps/index.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+const user = new User();
+const company = new Company();
+console.log(company)
+console.log(user)
+console.log(google)
+
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
