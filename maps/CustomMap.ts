8b4d4ace0f6b8b223c0daade556aa7d074d43e85
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-02-24 00:17:18 +0900

    Improve with Union type checker

--- maps/CustomMap.ts
+++ maps/CustomMap.ts
@@ -14,22 +14,31 @@ export class CustomMap {
     })
   }
 
-  addUserMarker(user: User): void {
+  // addUserMarker(user: User): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng
+  //     }
+  //   })
+  // }
+
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   })
+  addMarker(mappable: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     })
   }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng
-      }
-    })
 }
