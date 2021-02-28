11df7361cd6f1fa0dc97fc27210cca937eb8349b
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-03-01 00:02:35 +0900

    Sort String

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,11 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
-
+import { CharactersCollecion } from './CharactersCollecion';
 const numbersCollection = new NumbersCollection([15, -3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
+const character = new CharactersCollecion('aaCaxaX');
+const sorter2 = new Sorter(character);
+sorter2.sort();
+console.log(sorter2.collection)
