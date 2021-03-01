a2fff79bc2937c455ff3dc168d2ab5970390871e
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-03-01 21:09:20 +0900

    Load & Parse CSV

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
 import fs from 'fs';
+import { stringify } from 'node:querystring';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => {
+  return row.split(',')
+});
+console.log(matches);
+
+let manUnitedWins = 0
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man united' && match[5] === 'W') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins}`)
