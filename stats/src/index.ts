065e5b017b7e098f462245d812213176ad485abe
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-03-03 21:42:19 +0900

    Second refactor with composition pattern

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,22 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
-const reader = new MatchReader('football.csv');
-reader.read();
+import { CsvFileReader } from './CsvFileReader';
 
-const dateOfFirstMatch = reader.data[0][0];
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+//Create an instance of MatchReader and pass in something satisfying
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man united' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
-console.log(reader.data)
-console.log(`Man United won ${manUnitedWins}`)
+console.log(matchReader.matches);
+
+console.log(`Man United won ${manUnitedWins}`);
