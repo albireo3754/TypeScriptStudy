003bc2c76104c4fb9a916e9db1e311f612b4076e
윤상진 <yunsangjin@yunsangjin-ui-MacBookAir.local>
2021-03-16 22:47:01 +0900

    Fetch Data and render on screen

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+// import { Store } from 'redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export const App = () => {
-  return <div>Hi there!</div>;
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+export const _App = ({ todos, fetchTodos }: AppProps) => {
+  const onClick = (): void => {
+    fetchTodos();
+  };
+
+  const renderList = (): JSX.Element[] => {
+    console.log('what');
+    return todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  };
+  return (
+    <div>
+      <button onClick={onClick}>Fetch</button>
+      {renderList()}
+    </div>
+  );
 };
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
